e3b27bfdc1834a23d4ab6223e49c0fe5d493594e
LeonardoHuelvas <lejhubo01@hotmail.com>
2024-05-08 12:36:43 -0500

    autoinstaller chromedriver

--- tracker.py
+++ tracker.py
@@ -8,7 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
-import chromedriver_binary  # type: ignore # Agrega esta línea para importar chromedriver-binary
+import chromedriver_autoinstaller  # type: ignore # Importa la biblioteca de autoinstalación de ChromeDriver
+
+# Llama a la función de autoinstalación de chromedriver
+chromedriver_autoinstaller.install()
 
 def track_package(tracking_number):
     service = Service()  
