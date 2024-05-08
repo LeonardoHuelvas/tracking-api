2757086487a474f90f18ef7caf9670f3ada36b04
LeonardoHuelvas <lejhubo01@hotmail.com>
2024-05-07 20:49:26 -0500

    Driverchrome configurado

--- tracker.py
+++ tracker.py
@@ -1,4 +1,5 @@
 import os
+import stat
 import random
 import time
 from selenium import webdriver
@@ -10,7 +11,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
 
-# Carga las variables de entorno del archivo .env
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -18,15 +18,13 @@ def track_package(tracking_number):
     driver_path = os.getenv('CHROMEDRIVER_PATH')
     print(f"Ruta de Chromedriver: {driver_path}")
     
-    # Asegurarse que el path del chromedriver es accesible y los permisos son correctos
     if not os.path.exists(driver_path):
         raise Exception("El archivo chromedriver no existe en la ruta especificada.")
-    if not os.access(driver_path, os.X_OK):
-        raise Exception("El archivo chromedriver no tiene permisos de ejecución.")
+    os.chmod(driver_path, os.stat(driver_path).st_mode | stat.S_IEXEC)
 
     service = Service(executable_path=driver_path)
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # Ejecución sin interfaz gráfica
+    options.add_argument("--headless")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
@@ -38,25 +36,17 @@ def track_package(tracking_number):
     try:
         driver.get("https://www.zim.com/es/tools/track-a-shipment")
         time.sleep(random.randint(2, 5))
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
-        ).click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
         action = ActionChains(driver)
-        tracking_input = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "shipment-main-search-2"))
-        )
+        tracking_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "shipment-main-search-2")))
         action.move_to_element(tracking_input).click().perform()
         time.sleep(random.uniform(0.5, 1.5))
         tracking_input.send_keys(tracking_number)
         tracking_input.send_keys(Keys.RETURN)
         time.sleep(2)
-        follow_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary.chips-search-button"))
-        )
+        follow_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary.chips-search-button")))
         follow_button.click()
-        WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#trackShipment > div > div.routing-details"))
-        )
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#trackShipment > div > div.routing-details")))
         data_elements = driver.find_elements(By.CSS_SELECTOR, "#trackShipment > div > div.routing-details")
         details = [element.text for element in data_elements]
         return details
@@ -66,5 +56,4 @@ def track_package(tracking_number):
     finally:
         driver.quit()
 
-# Ejemplo de llamada a la función
-# print(track_package("ZIMUMER2451448"))
+# Ejemplo de llam
