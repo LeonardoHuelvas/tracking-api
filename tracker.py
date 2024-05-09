6567452841fce3c9368c23bfabbc07322b7ad4db
LeonardoHuelvas <lejhubo01@hotmail.com>
2024-05-08 19:18:19 -0500

    chromedriver_autoinstaller v1

--- tracker.py
+++ tracker.py
@@ -1,5 +1,3 @@
-import random
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,38 +6,62 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
+import os
 
 def track_package(tracking_number):
-    service = Service()  # No necesitas especificar la ruta de chromedriver, chromedriver_binary se encargará de ello
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    options.add_experimental_option('useAutomationExtension', False)
+  """  «»"Rastrea un paquete utilizando el sitio web de ZIM y devuelve los detalles de seguimiento.
 
-    driver = webdriver.Chrome(service=service, options=options)
+  Args:
+      tracking_number (str): El número de seguimiento del paquete.
+
+  Devuelve:
+      lista: Una lista de cadenas que contiene los detalles de seguimiento, o una lista vacía si hay un error.
+  «»»
+  """
+
+  # Get ChromeDriver path from environment variable
+  chromedriver_path = os.environ.get("CHROMEDRIVER_PATH")
+
+  if not chromedriver_path:
+    raise ValueError("CHROMEDRIVER_PATH environment variable not set!")
+
+  options = webdriver.ChromeOptions()
+  options.add_argument("--headless")
+  options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+  options.add_argument("--disable-blink-features=AutomationControlled")
+  options.add_experimental_option('excludeSwitches', ['enable-automation'])
+  options.add_experimental_option('useAutomationExtension', False)
+
+  driver = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=options)
+
+  try:
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
-    try:
-        driver.get("https://www.zim.com/es/tools/track-a-shipment")
-        time.sleep(random.randint(2, 5))
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
-        action = ActionChains(driver)
-        tracking_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "shipment-main-search-2")))
-        action.move_to_element(tracking_input).click().perform()
-        time.sleep(random.uniform(0.5, 1.5))
-        tracking_input.send_keys(tracking_number)
-        tracking_input.send_keys(Keys.RETURN)
-        time.sleep(2)
-        follow_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary.chips-search-button")))
-        follow_button.click()
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#trackShipment > div > div.routing-details")))
-        data_elements = driver.find_elements(By.CSS_SELECTOR, "#trackShipment > div > div.routing-details")
-        details = [element.text for element in data_elements]
-        return details
-    except Exception as e:
-        print("Error al cargar:", e)
-        return []
-    finally:
-        driver.quit()
+    driver.get("https://www.zim.com/es/tools/track-a-shipment")
+
+    # Use explicit waits with specific conditions for improved reliability
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
+
+    action = ActionChains(driver)
+    tracking_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "shipment-main-search-2")))
+    action.move_to_element(tracking_input).click().perform()
+
+    tracking_input.send_keys(tracking_number)
+    tracking_input.send_keys(Keys.RETURN)
+
+    follow_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary.chips-search-button")))
+    follow_button.click()
+
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#trackShipment > div > div.routing-details")))
+
+    data_elements = driver.find_elements(By.CSS_SELECTOR, "#trackShipment > div > div.routing-details")
+    details = [element.text for element in data_elements]
+
+    return details
+
+  except Exception as e:
+    print("Error al cargar:", e)
+    return []
+
+  finally:
+    driver.quit()
